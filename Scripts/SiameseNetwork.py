a6e17a8ba37a5b3380e166e1e5c8c796b0a38b60
pascaloneroof <pascal@lonerooftop.com>
2017-05-20 12:14:34 +0200

    Separate prediction of submission per blocks to avoid memory overflow

--- Scripts/SiameseNetwork.py
+++ Scripts/SiameseNetwork.py
@@ -61,7 +61,7 @@ df['q2_feats'] = list(vecs2)
 df = df.reindex(np.random.permutation(df.index))
 
 # set number of train and test instances
-num_train = int(df.shape[0] * 0.90)
+num_train = int(df.shape[0] * 0.98)
 num_test = df.shape[0] - num_train
 print("Number of training pairs: %i" % (num_train))
 print("Number of testing pairs: %i" % (num_test))
@@ -99,18 +99,22 @@ net = create_network(300)
 
 # train
 # optimizer = SGD(lr=1, momentum=0.8, nesterov=True, decay=0.004)
-optimizer = Adam(lr=0.1,decay= 0.005)
+optimizer = Adam(lr=0.17,decay= 0.005)
 net.compile(loss='binary_crossentropy', optimizer=optimizer)
 net.fit([X_train[:, 0, :], X_train[:, 1, :]], Y_train,
         validation_data=([X_test[:, 0, :], X_test[:, 1, :]], Y_test),
-        batch_size=128, epochs=10, shuffle=True, )
+        batch_size=128, epochs=20, shuffle=True,class_weight={1: 0.46, 0: 1.32})
 
 print('Preparing Submission Data')
 
 df_test= pd.read_csv('test.csv')
 questions = list(df_test['question1'].values.astype(str)) + list(df_test['question2'].values.astype(str))
 tfidf.transform(questions)
+# tfidf = TfidfVectorizer(lowercase=False, )
+# tfidf.fit_transform(questions)
 word2tfidf = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
+
+
 
 vecs1 = []
 for qu in tqdm(list(df_test['question1'])):
@@ -151,19 +155,52 @@ for qu in tqdm(list(df_test['question2'])):
 df_test['q2_feats'] = list(vecs2)
 
 
-b = [a[None, :] for a in list(df_test['q1_feats'].values)]
+## other alternative
+seperators= [750000,1500000]
+
+X_sub1 = np.zeros([750000, 2, 300])
+b = [a[None, :] for a in list(df_test['q1_feats'].iloc[:seperators[0]].values)]
 q1_feats = np.concatenate(b, axis=0)
-
-
-b = [a[None, :] for a in list(df_test['q2_feats'].values)]
+b = [a[None, :] for a in list(df_test['q2_feats'].iloc[:seperators[0]].values)]
 q2_feats = np.concatenate(b, axis=0)
+X_sub1[:, 0, :] = q1_feats
+X_sub1[:, 1, :] = q2_feats
+del b
+del q1_feats
+del q2_feats
 
-X_sub = np.zeros([df_test.shape[0], 2, 300])
-X_sub[:, 0, :] = q1_feats
-X_sub[:, 1, :] = q2_feats
+
+X_sub2 = np.zeros([750000, 2, 300])
+b = [a[None, :] for a in list(df_test['q1_feats'].iloc[seperators[0]:seperators[1]].values)]
+q1_feats = np.concatenate(b, axis=0)
+b = [a[None, :] for a in list(df_test['q2_feats'].iloc[seperators[0]:seperators[1]].values)]
+q2_feats = np.concatenate(b, axis=0)
+X_sub2[:, 0, :] = q1_feats
+X_sub2[:, 1, :] = q2_feats
+del b
+del q1_feats
+del q2_feats
 
 
-y_submission = net.predict([X_sub[:, 0, :],X_sub[:, 1, :]])
-df_test['is_duplicate'] = y_submission
-submission = df_test[['test_id','is_duplicate']]
-submission = submission.to_csv('/Users/pascalsitbon/work/Kaggle/pred.csv',index=False)
+X_sub3 = np.zeros([845796, 2, 300])
+b = [a[None, :] for a in list(df_test['q1_feats'].iloc[seperators[1]:].values)]
+q1_feats = np.concatenate(b, axis=0)
+b = [a[None, :] for a in list(df_test['q2_feats'].iloc[seperators[1]:].values)]
+q2_feats = np.concatenate(b, axis=0)
+X_sub3[:, 0, :] = q1_feats
+X_sub3[:, 1, :] = q2_feats
+del b
+del q1_feats
+del q2_feats
+
+testPredictions1 = net.predict([X_sub1[:, 0, :],X_sub1[:, 1, :]])
+testPredictions2 = net.predict([X_sub2[:, 0, :],X_sub2[:, 1, :]])
+testPredictions3 = net.predict([X_sub3[:, 0, :],X_sub3[:, 1, :]])
+testPredictions = np.hstack((testPredictions1,testPredictions2,testPredictions3))
+
+submissionName = 'Siamese'
+
+submission = pd.DataFrame()
+submission['test_id'] = df_test['test_id']
+submission['is_duplicate'] = testPredictions
+submission.to_csv(submissionName + '.csv', index=False)
